feaeb332a6a3f74b18a694411f5d8945f507f1e9
SevenTwelve712 <seventwwwelve712@gmail.com>
2025-12-14 22:57:34 +0300

    add to server 2 var sort, add CMake support

---
+++ build/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/seventwelve712/Education/CProjectActivities/server.cpp" "CMakeFiles/server.dir/server.cpp.o" "gcc" "CMakeFiles/server.dir/server.cpp.o.d"
+  "/home/seventwelve712/Education/CProjectActivities/sort6var.cpp" "CMakeFiles/server.dir/sort6var.cpp.o" "gcc" "CMakeFiles/server.dir/sort6var.cpp.o.d"
+  "" "server" "gcc" "CMakeFiles/server.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
